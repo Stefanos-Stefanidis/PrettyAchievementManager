195b5504cab2a07fab8a75ce6a0512ce1b38f19e
steve stevendis <s.stevenidis@gmail.com>
2016-07-17 23:20:14 +0300

    fornt end for adding  and display achievemets

--- www/js/achievmentsManagment.js
+++ www/js/achievmentsManagment.js
@@ -1,23 +1,26 @@
 
 //controller of a static model
 app.controller('achievments_Ctrl', function ($scope) {
+
   $scope.achievments = [
-    { name: 'Achievment1', progress: '10', description: '', points: '' },
-    { name: 'Achievment2', progress: '50', description: '', points: '' },
-    { name: 'Achievment3', progress: '100', description: '', points: '' }
+    { name: 'Achievment1', progress: '10', description: 'decs1', points: '10' },
+    { name: 'Achievment2', progress: '50', description: 'desc2', points: '30' },
+    { name: 'Achievment3', progress: '100', description: 'desc3', points: '100' }
   ];
+
+  
+/*  $scope.addAchievement = function() {
+      $scope.achievments.push({name:$scope.name1,progress:$scope.name1,description:$scope.name1,points:$scope.name1});
+      $scope.name1 = '';
+  };*/
 });
 
 
-
-
-// den exo katalavei akoma ti kanei
-app.controller("getValue", function ($scope, $rootScope, $element, $interval) {
+app.controller("getValue", function ($scope, $element) {
   $scope.min = 0;
   $scope.max = 100;
   $scope.modelValue = 30;
-
-  $interval(function () {
-    $scope.inputValue = $element.find('input').val();
-  }, 250);
+  
+  $scope.inputValue = $element.find('input').val();
+ 
 });
